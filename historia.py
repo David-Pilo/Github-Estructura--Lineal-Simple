b1a7cb3d1a9c04213cd945e0518a8773397de56b
David <david.pilo@uip.edu.pa>
2026-03-07 13:03:30 -0500

    se inicio el .py

---
+++ historia.py
@@ -0,0 +1,5 @@
+nombre = input("Introdusca su nombre ")
+edad = input("Introdusca su edad ")
+residencia = input("Introdusca su ciudad residencia ")
+
+print (f"Hola mi nombre es {nombre} tengo {edad} años y soy de {residencia}")
